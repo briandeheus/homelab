4d0019b08fd607f57626832e6e073715e51288e6
Brian de Heus <me@brian.jp>
2024-08-31 21:34:12 +0400

    Adds commit hash to landing page

--- server.py
+++ server.py
@@ -2,10 +2,15 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from lib.utils import get_short_commit_hash
+
 templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 
 
 @app.get("/")
 def landing(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(request=request, name="base.html")
+    github_commit_hash = get_short_commit_hash()
+    return templates.TemplateResponse(
+        request=request, name="base.html", context={"commit_hash": github_commit_hash}
+    )
