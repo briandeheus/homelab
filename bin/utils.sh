d97c40ad2b58aeee39208c1fe7f6e30e0e8d216a
Brian de Heus <me@brian.jp>
2024-08-31 21:27:33 +0400

    When we su we should exit

--- bin/utils.sh
+++ bin/utils.sh
@@ -14,6 +14,7 @@ install_homelab() {
     python -m venv .venv
     source .venv/bin/activate
     pip install -r requirements.txt
+    exit
 EOF
 }
 
@@ -28,5 +29,6 @@ update_homelab() {
     python -m venv .venv
     source .venv/bin/activate
     pip install -r requirements.txt
+    exit
 EOF
 }
